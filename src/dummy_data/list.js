526808f4d77784a0e621b5b856eb71465f78f25f
Andrew Mirshafiee <andrewmirs@csu.fullerton.edu>
2018-11-02 17:55:17 -0700

    added titles from dummy data to DOM table

---
+++ src/dummy_data/list.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        title: 'Go to store',
+        details: 'Get toast, avacados, caviar, dill pickles, and toilet papel',
+        id: '001'
+    },
+    {
+        title: 'Pick up neice',
+        details: 'She gets out of pre-school at 3pm',
+        id: '002'
+    },
+    {
+        title: 'Feed the llama',
+        details: 'Henry gets hangry after noon so we might be f***ed',
+        id: '003'
+    },
+    {
+        title: 'Soak in the Misses',
+        details: "You don't know what you're missing",
+        id: '004'
+    }
+]
